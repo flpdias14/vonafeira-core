111da4e7ea89ee8949534df9eae43905b4e6fb33
Felipe Dias de Oliveira <felipe@Winterfell>
2018-08-02 12:37:48 -0300

    Criação de Eventos, Alteração no Grupo de Consumo

--- database/migrations/2018_07_29_020144_create_eventos_table.php
+++ database/migrations/2018_07_29_020144_create_eventos_table.php
@@ -19,8 +19,8 @@ class CreateEventosTable extends Migration
             $table->foreign('grupoconsumo_id')->references('id')->on('grupo_consumos');
             $table->integer('coordenador_id')->unsigned();
             $table->foreign('coordenador_id')->references('id')->on('users');
-            $table->date('data_inicio');
-            $table->date('data_fim');
+            $table->date('data_inicio_pedidos');
+            $table->date('data_fim_pedidos');
             $table->date('data_evento');
             $table->string('hora_evento');
             $table->timestamps();
