4dbaac5ac09f4d1134523878903b470c4ec4975b
Felipe Oliveira <flpdias14@gmail.com>
2018-08-16 15:19:31 -0300

    Formatação de preço nos itens do pedido

--- resources/views/pedido/itensPedido.blade.php
+++ resources/views/pedido/itensPedido.blade.php
@@ -7,24 +7,39 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Itens do Pedido</div>
                 <div class="panel-body">
-                    <table class="table table-hover">         
-                        <tr>
-                            <th>Produto</th>
-                            <th>Quantidade</th>
-                            <th>Unidade</th>
-                            <th>Preço</th>
-                            <th>Preço Total do Item</th>
-                        </tr>
-                        
+                    <table class="table table-hover"> 
+                        <thead>        
+                            <tr>
+                                <th>Produto</th>
+                                <th>Quantidade</th>
+                                <th>Unidade</th>
+                                <th>Preço</th>
+                                <th>Preço Total do Item</th>
+                            </tr>
+                        </thead>
+                        @php($total = 0)
+                        <tbody>
                         @foreach ($itensPedido as $itemPedido)
                         <tr>
                             <td>{{ $itemPedido->nome_produto }}</td>
                             <td>{{ $itemPedido->quantidades }}</td>
                             <td>{{ $itemPedido->unidade_venda }}</td>
-                            <td>{{ $itemPedido->preco }}</td>
-                            <td>{{ $itemPedido->preco * $itemPedido->quantidades }}</td>
+                            <td>{{ 'R$'.number_format($itemPedido->preco, 2) }}</td>
+                            <td>{{ 'R$'.number_format($itemPedido->preco * $itemPedido->quantidades, 2) }}</td>
+                            @php($total = $total + $itemPedido->preco * $itemPedido->quantidades)
                         </tr>
                         @endforeach
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th><strong>Total</strong></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th>{{'R$'.number_format($total, 2)}}</th>
+                                <th></th>
+                            </tr>
+                        </tfoot>
                     </table>
                 </div>
                 <div class="panel-footer">
